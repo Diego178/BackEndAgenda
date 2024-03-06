fd0e3af1a4d57bcba8f7ca6941434b60720f1ac0
Angel Fabrizio Franyutti Pulido <afabri24@gmail.com>
2024-03-06 13:07:41 -0600

    Update settings.py

--- servicioAgenda/settings.py
+++ servicioAgenda/settings.py
@@ -110,9 +110,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_TLS = False
+EMAIL_PORT = 465
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = True
 
