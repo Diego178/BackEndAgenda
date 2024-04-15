02ec00e93afcf0b927f379ecb25a714b7b51ded0
Angel Fabrizio Franyutti Pulido <afabri24@gmail.com>
2024-04-15 12:25:55 -0600

    Update views.py

--- asesorias/views.py
+++ asesorias/views.py
@@ -103,15 +103,14 @@ def registrarAsesoria(request):
     fecha = request.data.get('fecha')
     idAsesor = request.data.get('idAsesor')
     idDiaHora = request.data.get('idDiaHora')
-    idUsuario = request.data.get('idUsuario')
     token = request.data.get('token')
 
 
-    valido, mensaje = verificarToken(token)
+    valido, mensaje = verificarTokenUsuario(token)
     if not valido:
         return Response({'mensaje': mensaje, "error": True}, status=200)
 
-    if tipo is not None and tema is not None and fecha is not None and idAsesor is not None and idDiaHora is not None and idUsuario is not None:
+    if tipo is not None and tema is not None and fecha is not None and idAsesor is not None and idDiaHora is not None and mensaje is not None:
         if(tipo != 'asesoria' and tipo != 'oral'):
             return Response({'mensaje': 'Tipo no valido', "error": True}, status=400)
         
@@ -132,7 +131,7 @@ def registrarAsesoria(request):
             return Response({'mensaje': 'Error, el horario elegido no existe en la base de datos.', "error": True}, status=200)
         
         try:
-            usuario = Usuario.objects.get(id_usuario=idUsuario)
+            usuario = Usuario.objects.get(id_usuario=mensaje)
         except ObjectDoesNotExist:
             return Response({'mensaje': 'Error, el usuario no existe en la base de datos.', "error": True}, status=200)
         
