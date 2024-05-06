43d1176fad42570c6131abbb4a9c96f980d8a8b2
Diego178 <89479225+Diego178@users.noreply.github.com>
2024-05-06 16:47:40 -0600

    make it better4

--- asesores/models.py
+++ asesores/models.py
@@ -13,14 +13,6 @@ class Asesor(models.Model):
         managed = False
         db_table = 'asesor'
 
-class Curso(models.Model):
-    id_curso = models.AutoField(primary_key=True)
-    nombrecurso = models.CharField(db_column='nombreCurso', max_length=20, blank=True, null=True)  # Field name made lowercase.
-    idasesor = models.ForeignKey(Asesor, models.CASCADE, db_column='idAsesor', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'curso'
 
 class Datosreunionvirtual(models.Model):
     id_datosreunion = models.AutoField(primary_key=True)
