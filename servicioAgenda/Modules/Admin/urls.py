44632c0aad1af1f00c07d96aa28a3ef87a1540d0
Diego178 <89479225+Diego178@users.noreply.github.com>
2024-06-12 11:49:35 -0600

    admin dashboard

--- servicioAgenda/Modules/Admin/urls.py
+++ servicioAgenda/Modules/Admin/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("obtenerAsesores/", views.obtenerAsesores)
+    path("obtenerAsesores/", views.obtenerAsesores),
+    path("actualizarAsesor/", views.actualizarAsesor)
 ]
